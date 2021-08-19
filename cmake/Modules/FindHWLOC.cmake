77df05453d26860584e64c4023420f449f918a05
Michael Hirsch <scivision@users.noreply.github.com>
2021-08-19 18:15:17 -0400

    FindHWLOC: emit version

--- cmake/Modules/FindHWLOC.cmake
+++ cmake/Modules/FindHWLOC.cmake
@@ -27,31 +27,50 @@ HWLOC_INCLUDE_DIRS
 include(CheckSymbolExists)
 
 find_package(PkgConfig)
-pkg_check_modules(pc_hwloc hwloc)
+pkg_search_module(pc_hwloc hwloc)
 
 # NOTE: the "lib*" are for Windows Intel compiler.
 # CMake won't look for lib prefix automatically.
 
 find_library(HWLOC_LIBRARY
-             NAMES hwloc libhwloc
-             NAMES_PER_DIR
-             HINTS ${pc_hwloc_LIBRARY_DIRS} ${pc_hwloc_LIBDIR})
+  NAMES hwloc libhwloc
+  NAMES_PER_DIR
+  HINTS ${pc_hwloc_LIBRARY_DIRS} ${pc_hwloc_LIBDIR})
 
 find_path(HWLOC_INCLUDE_DIR
-          NAMES hwloc.h
-          HINTS ${pc_hwloc_INCLUDE_DIRS})
+  NAMES hwloc.h
+  HINTS ${pc_hwloc_INCLUDE_DIRS})
 
 if(HWLOC_LIBRARY AND HWLOC_INCLUDE_DIR)
-set(CMAKE_REQUIRED_INCLUDES ${HWLOC_INCLUDE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${HWLOC_LIBRARY})
-check_symbol_exists(hwloc_topology_load hwloc.h HWLOC_TOPO_LOAD)
+  set(CMAKE_REQUIRED_INCLUDES ${HWLOC_INCLUDE_DIR})
+  set(CMAKE_REQUIRED_LIBRARIES ${HWLOC_LIBRARY})
+  check_symbol_exists(hwloc_topology_load hwloc.h HWLOC_TOPO_LOAD)
+endif()
+
+if(HWLOC_TOPO_LOAD)
+  find_file(hwloc_conf
+    NAMES config.h
+    HINTS ${HWLOC_INCLUDE_DIR} ${pc_hwloc_INCLUDE_DIRS}
+    PATH_SUFFIXES hwloc/autogen
+    DOC "HWLOC configuration header"
+  )
+
+  if(hwloc_conf)
+    file(STRINGS ${hwloc_conf} _def
+    REGEX "^[ \t]*#[ \t]*define[ \t]+HWLOC_VERSION[ \t]+" )
+    if("${_def}" MATCHES "HWLOC_VERSION[ \t]+\"([0-9]+\\.[0-9]+\\.[0-9]+)?\"")
+      set(HWLOC_VERSION "${CMAKE_MATCH_1}")
+    endif()
+  endif()
+endif()
+
 set(CMAKE_REQUIRED_INCLUDES)
 set(CMAKE_REQUIRED_LIBRARIES)
-endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HWLOC
-    REQUIRED_VARS HWLOC_LIBRARY HWLOC_INCLUDE_DIR HWLOC_TOPO_LOAD)
+    REQUIRED_VARS HWLOC_LIBRARY HWLOC_INCLUDE_DIR HWLOC_TOPO_LOAD
+    VERSION_VAR HWLOC_VERSION)
 
 if(HWLOC_FOUND)
 set(HWLOC_LIBRARIES ${HWLOC_LIBRARY})
@@ -66,4 +85,4 @@ if(NOT TARGET HWLOC::HWLOC)
 endif()
 endif(HWLOC_FOUND)
 
-mark_as_advanced(HWLOC_INCLUDE_DIR HWLOC_LIBRARY)
+mark_as_advanced(HWLOC_INCLUDE_DIR HWLOC_LIBRARY HWLOC_TOPO_LOAD)
