2fb1246e654d2b0831ae2174ffd216cee6006ecd
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-14 18:21:12 -0500

    return fastest CPU count if heterogenous CPU

--- get_cpu_hwloc.c
+++ get_cpu_hwloc.c
@@ -7,6 +7,16 @@ int cpu_count_c(void){
 
 hwloc_topology_t sTopology;
 
+// best estimate of fast physical CPU core count
+int nPhysicalCPU;
+
+// CPU kind efficiency variables
+hwloc_bitmap_t cpuset = hwloc_bitmap_alloc();
+unsigned nr_infos;
+struct hwloc_info_s *infos;
+int efficiency;
+
+
 if (hwloc_topology_init(&sTopology) != 0){
   fprintf(stderr, "hwloc: could not init topology\n");
   return -1;
@@ -19,19 +29,31 @@ if (hwloc_topology_load(sTopology) != 0){
 // see if CPU is heterogeneous (more than one CPU core type, e.g. fast/slow like Apple Silicon)
 int Ncpu_kind = hwloc_cpukinds_get_nr(sTopology, 0);
 if (Ncpu_kind > 1){
-  printf("CPU is heterogeneous\n");
+  printf("CPU is heterogeneous: %d kinds of CPU cores detected.\n", Ncpu_kind);
+
+  for (int i = 0; i < Ncpu_kind; i++){
+    if(hwloc_cpukinds_get_info(sTopology, i, cpuset, &efficiency, &nr_infos, &infos, 0) != 0){
+      fprintf(stderr, "hwloc: could not get CPU kinds info.\n");
+      return -1;
+    }
+    printf("CPU kind %d efficiency score: %d\n", i, efficiency);
+  }
+
+  nPhysicalCPU = nr_infos;
+}
+else
+{
+  // https://www.open-mpi.org/projects/hwloc/doc/v2.4.0/a00154.php#gacd37bb612667dc437d66bfb175a8dc55
+  nPhysicalCPU = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_CORE);
+  if (nPhysicalCPU < 1) {
+    // assume hyperthreading / 2
+    nPhysicalCPU = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_PU) / 2;
+    fprintf(stderr, "hwloc: fallback to HWLOC_OBJ_PU count/2: HWLOC_OBJ_CORE count not available\n");
+  }
+
+  hwloc_topology_destroy(sTopology);
 }
 
-// https://www.open-mpi.org/projects/hwloc/doc/v2.4.0/a00154.php#gacd37bb612667dc437d66bfb175a8dc55
-int nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_CORE);
-if (nCore < 1) {
-  // assume hyperthreading / 2
-  nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_PU) / 2;
-  fprintf(stderr, "hwloc: fallback to HWLOC_OBJ_PU count/2: HWLOC_OBJ_CORE count not available\n");
-}
-
-hwloc_topology_destroy(sTopology);
-
-return nCore;
+return nPhysicalCPU;
 
 }
