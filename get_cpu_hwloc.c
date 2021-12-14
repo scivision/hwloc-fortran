ca9719bb74db3c3034bd53c50c4b7cefa89adc97
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-14 17:58:47 -0500

    query CPU kind count, requires HWLOC >= 2.4
    FindHWLOC: handle version range

--- get_cpu_hwloc.c
+++ get_cpu_hwloc.c
@@ -5,26 +5,33 @@
 
 int cpu_count_c(void){
 
-  hwloc_topology_t sTopology;
+hwloc_topology_t sTopology;
 
-  if (hwloc_topology_init(&sTopology) != 0){
-    fprintf(stderr, "hwloc: could not init topology\n");
-    return -1;
-  }
-  if (hwloc_topology_load(sTopology) != 0){
-    fprintf(stderr, "hwloc: could not load topology\n");
-    return -1;
-  }
+if (hwloc_topology_init(&sTopology) != 0){
+  fprintf(stderr, "hwloc: could not init topology\n");
+  return -1;
+}
+if (hwloc_topology_load(sTopology) != 0){
+  fprintf(stderr, "hwloc: could not load topology\n");
+  return -1;
+}
+
+// see if CPU is heterogeneous (more than one CPU core type, e.g. fast/slow like Apple Silicon)
+int Ncpu_kind = hwloc_cpukinds_get_nr(sTopology, 0);
+if (Ncpu_kind > 1){
+  printf("CPU is heterogeneous\n");
+}
+
 // https://www.open-mpi.org/projects/hwloc/doc/v2.4.0/a00154.php#gacd37bb612667dc437d66bfb175a8dc55
-  int nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_CORE);
-  if (nCore < 1) {
-    // assume hyperthreading / 2
-    nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_PU) / 2;
-    fprintf(stderr, "hwloc: fallback to HWLOC_OBJ_PU count/2: HWLOC_OBJ_CORE count not available\n");
-  }
+int nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_CORE);
+if (nCore < 1) {
+  // assume hyperthreading / 2
+  nCore = hwloc_get_nbobjs_by_type(sTopology, HWLOC_OBJ_PU) / 2;
+  fprintf(stderr, "hwloc: fallback to HWLOC_OBJ_PU count/2: HWLOC_OBJ_CORE count not available\n");
+}
 
-  hwloc_topology_destroy(sTopology);
+hwloc_topology_destroy(sTopology);
 
-  return nCore;
+return nCore;
 
 }
